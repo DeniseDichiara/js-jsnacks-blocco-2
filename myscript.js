dbfcd366d7cf172f3c27b34730cbd78fd17b24ca
Denise-Dichiara <dichiara.d@gmail.com>
2023-05-19 16:47:47 +0200

    sum

---
+++ myscript.js
@@ -0,0 +1,13 @@
+let emptyList = []
+
+let userNumber = parseInt(prompt('Enter a number'))
+
+let sum = 50
+
+let i = 0
+
+while( (i < emptyList.length) && (sum === false) ){
+    console.log(Empty[userNumber])
+
+    if(Empty[userNumber])
+}

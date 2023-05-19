95e3a5740c105bc13c15e5e92cccb66ca5ec6e1d
Denise-Dichiara <dichiara.d@gmail.com>
2023-05-19 17:42:33 +0200

    index

--- myscript.js
+++ myscript.js
@@ -2,12 +2,24 @@ let emptyList = []
 
 let userNumber = parseInt(prompt('Enter a number'))
 
-let sum = 50
+let sum = false
 
 let i = 0
 
-while( (i < emptyList.length) && (sum === false) ){
+while( (userNumber < emptyList.length) && (sum === false) ){
     console.log(Empty[userNumber])
 
     if(Empty[userNumber])
+}
+
+
+//? Genara numeri random da 1 a 100
+let randomNumber = Math.floor(Math.random() * (100 - 1) + 1);
+
+let userNumberGuess = parseInt(prompt('Guess the number'))
+
+let rightNumber = 0
+
+while(userNumberGuess !== randomNumber){
+    console.log (userNumber = false)
 }
